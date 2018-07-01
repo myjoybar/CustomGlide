ab5ea62626259bf23c135144140fbd8704994a30
myjoybar <myjoybar@gmail.com>
2018-07-01 22:21:03 +0800

    add SoftReference

--- library/src/main/java/com/joy/glide/library/cache/memory/MemoryLruCache.java
+++ library/src/main/java/com/joy/glide/library/cache/memory/MemoryLruCache.java
@@ -8,6 +8,7 @@ import android.util.LruCache;
 import com.joy.glide.library.cache.key.Key;
 import com.joy.glide.library.utils.GLog;
 
+import java.lang.ref.SoftReference;
 import java.util.Collection;
 
 /**
@@ -16,58 +17,66 @@ import java.util.Collection;
 
 public class MemoryLruCache implements MemoryCache {
 
-	private static LruCache<Key, Bitmap> mMemoryCache;
+    private static LruCache<Key, SoftReference<Bitmap>> mMemoryCache;
 
-	private int availableMemoryPercent;
-	public MemoryLruCache() {
-		this(50);
-	}
+    private int availableMemoryPercent;
 
-	public MemoryLruCache(int availableMemoryPercent) {
-		if (availableMemoryPercent <= 0 || availableMemoryPercent >= 100) {
-			throw new IllegalArgumentException("availableMemoryPercent must be in range (0 < % < 100)");
-		}
-		GLog.printInfo("availableMemoryPercent= "+availableMemoryPercent);
-		int maxMemory = (int) Runtime.getRuntime().maxMemory();
-		int cacheSize = maxMemory *availableMemoryPercent/ 100;
-		mMemoryCache = new LruCache<Key, Bitmap>(cacheSize) {
-			@Override
-			protected int sizeOf(Key key, Bitmap bitmap) {
-				return bitmap.getByteCount();
-			}
-		};
-	}
+    public MemoryLruCache() {
+        this(50);
+    }
 
-	@Override
-	public boolean put(Key key, Bitmap value) {
-		if (get(key) == null) {
-			mMemoryCache.put(key, value);
-			return true;
-		}
-		return false;
+    public MemoryLruCache(int availableMemoryPercent) {
+        if (availableMemoryPercent <= 0 || availableMemoryPercent >= 100) {
+            throw new IllegalArgumentException("availableMemoryPercent must be in range (0 < % < " +
+                    "" + "100)");
+        }
+        GLog.printInfo("availableMemoryPercent= " + availableMemoryPercent);
+        int maxMemory = (int) Runtime.getRuntime().maxMemory();
+        int cacheSize = maxMemory * availableMemoryPercent / 100;
+        mMemoryCache = new LruCache<Key, SoftReference<Bitmap>>(cacheSize) {
+            @Override
+            protected int sizeOf(Key key, SoftReference<Bitmap> bitmap) {
+                return bitmap.get().getByteCount();
+            }
+        };
+    }
 
-	}
+    @Override
+    public boolean put(Key key, Bitmap value) {
+        if (get(key) == null) {
+            mMemoryCache.put(key, new SoftReference<Bitmap>(value));
+            return true;
+        }
+        return false;
 
-	@Override
-	public Bitmap get(Key key) {
-		return mMemoryCache.get(key);
-	}
+    }
 
-	@Override
-	public Bitmap remove(Key key) {
-		return mMemoryCache.remove(key);
-	}
+    @Override
+    public Bitmap get(Key key) {
+        if (key != null) {
+            return mMemoryCache.get(key).get();
+        }
+        return null;
+    }
 
-	@Override
-	public Collection<Key> keys() {
-		return null;
-	}
+    @Override
+    public Bitmap remove(Key key) {
+        if (key != null) {
+            return mMemoryCache.remove(key).get();
+        }
+        return null;
+    }
 
-	@RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
-	@Override
-	public void clear() {
-		mMemoryCache.trimToSize(-1);
-	}
+    @Override
+    public Collection<Key> keys() {
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void clear() {
+        mMemoryCache.trimToSize(-1);
+    }
 
 
 }
